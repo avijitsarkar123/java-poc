49aacec29ea3fe1e1ae42686aa126c473f9f294c
Avijit Sarkar <avijitsarkar123@gmail.com>
2013-09-21 20:06:23 -0400

    Added Comments

--- OnlineStore/src/com/avijit/poc/onlinestore/data/dao/ManufacturerDAO.java
+++ OnlineStore/src/com/avijit/poc/onlinestore/data/dao/ManufacturerDAO.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 import com.avijit.poc.onlinestore.business.entity.Manufacturer;
 
+/**
+ * Manufacturer DAO
+ * 
+ * @author avijit
+ *
+ */
 public interface ManufacturerDAO {
     public List<Manufacturer> getManufacturerList();
 }
